76a08f6c8d1dd4b3c3c4650e4406d90ee76bf6b8
baiyi <baiyitongxue@qq.com>
2025-04-02 19:55:04 +0800

    init repo

--- build/docs/javadoc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"com.baiyi.passkey","c":"PassKey","l":"onDisable()"},{"p":"com.baiyi.passkey","c":"PassKey","l":"onEnable()"},{"p":"com.baiyi.passkey","c":"PassKey","l":"PassKey()","u":"%3Cinit%3E()"}];updateSearchResults();
